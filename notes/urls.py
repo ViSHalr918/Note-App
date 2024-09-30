5b0ef13b4167b798d513ea99ed206f6225dbe088
vishal <vishalr9395@gmail.com>
2024-09-30 14:28:33 +0530

    noteapp

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,11 @@
 
 from django.urls import path
+from notes import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns=[
+    path("register/",views.UsercreationView.as_view()),
+
+    path("token/",ObtainAuthToken.as_view())
     
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from notes.serializers import UserSerializer
+from notes.models import User
+from rest_framework import generics
+from rest_framework.response import Response
 
 # Create your views here.
+
+class UsercreationView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self,request,*args,**kwargs):
+
+        serializer_instance = UserSerializer(data=request.data)
+
+        if serializer_instance.is_valid():
+
+            data = serializer_instance.validated_data
+
+            User.objects.create_user(**data)
+
+            return Response(data=serializer_instance.data)
+        
+        else:
+            return Response(serializer_instance.errors)
